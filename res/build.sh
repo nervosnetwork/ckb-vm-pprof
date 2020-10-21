c8f0ae85c84c1191198041822bc738eed113a159
mohanson <mohanson@outlook.com>
2020-10-21 23:42:17 +0800

    Add initial commit

---
+++ res/build.sh
@@ -0,0 +1,15 @@
+set -e
+
+if [ -z "$RISCV" ]; then
+    echo "Environment variable is not set: RISCV"
+    exit
+fi
+
+build() {
+    $RISCV/bin/riscv64-unknown-elf-gcc -g -o res/$1 res/$1.c
+    $RISCV/bin/riscv64-unknown-elf-objdump -d res/$1 > res/$1.objdump
+    $RISCV/bin/riscv64-unknown-elf-readelf -a res/$1 > res/$1.readelf
+    dwarfdump res/$1 > res/$1.dwarfdump
+}
+
+build fib
---
+++ src/main.rs
@@ -0,0 +1,205 @@
+use std::borrow::Cow;
+use std::cell::RefCell;
+use std::path::Path;
+use std::rc::Rc;
+
+use ckb_vm::{CoreMachine, Memory, Register};
+
+mod cost_model;
+
+#[allow(dead_code)]
+fn sprint_loc_file_line(loc: &Option<addr2line::Location>) -> String {
+    if let Some(ref loc) = *loc {
+        let mut list = vec![];
+        let file = loc.file.as_ref().unwrap();
+        let path = Path::new(file);
+        list.push(path.display().to_string());
+        if let Some(line) = loc.line {
+            list.push(format!("{}", line));
+        } else {
+            list.push(String::from("??"));
+        }
+        list.join(":")
+    } else {
+        String::from("??:??")
+    }
+}
+
+fn sprint_loc_file(loc: &Option<addr2line::Location>) -> String {
+    if let Some(ref loc) = *loc {
+        let file = loc.file.as_ref().unwrap();
+        let path = Path::new(file);
+        path.display().to_string()
+    } else {
+        String::from("??")
+    }
+}
+
+#[allow(dead_code)]
+fn sprint_fun(
+    mut frame_iter: addr2line::FrameIter<
+        addr2line::gimli::EndianReader<addr2line::gimli::RunTimeEndian, std::rc::Rc<[u8]>>,
+    >,
+) -> String {
+    let mut stack: Vec<String> = vec![String::from("??")];
+    loop {
+        let frame = frame_iter.next().unwrap();
+        if frame.is_none() {
+            break;
+        }
+        let frame = frame.unwrap();
+        let function = frame.function.unwrap();
+        let function_name = String::from(addr2line::demangle_auto(
+            Cow::from(function.raw_name().unwrap()),
+            function.language,
+        ));
+
+        stack.push(function_name)
+    }
+    stack.last().unwrap().to_string()
+}
+
+// Use tree structure to store ckb vm's runtime data. At present, we care about cycles, but we may add other things in
+// the future, for example, the number of times a certain instruction appears.
+#[derive(Clone, Debug)]
+struct PProfRecordTreeNode {
+    name: String, // FILENAME + FUNCTION_NAME as expected.
+    childs: Vec<Rc<RefCell<PProfRecordTreeNode>>>,
+    cycles: u64,
+}
+
+impl PProfRecordTreeNode {
+    // Create a new PProfRecordTreeNode with a user defined name(e.g. Function name).
+    fn root() -> Self {
+        Self {
+            name: String::from("??:??"),
+            childs: vec![],
+            cycles: 0,
+        }
+    }
+
+    fn display_flamegraph(&self, prefix: &str) -> String {
+        let prefix_name = prefix.to_owned() + self.name.as_str();
+        let mut r = format!("{} {}\n", prefix_name, self.cycles);
+        for e in &self.childs {
+            r.push_str(
+                e.borrow()
+                    .display_flamegraph(&(prefix_name.as_str().to_owned() + "; "))
+                    .as_str(),
+            );
+        }
+        return r;
+    }
+}
+
+// Main handler.
+struct PProfLogger {
+    atsl_context: addr2line::Context<
+        addr2line::gimli::EndianReader<addr2line::gimli::RunTimeEndian, std::rc::Rc<[u8]>>,
+    >,
+    tree_root: Rc<RefCell<PProfRecordTreeNode>>,
+    tree_node: Rc<RefCell<PProfRecordTreeNode>>,
+    tree_parent: Vec<Rc<RefCell<PProfRecordTreeNode>>>,
+}
+
+impl PProfLogger {
+    fn new(filename: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let file = std::fs::File::open(filename)?;
+        let mmap = unsafe { memmap::Mmap::map(&file)? };
+        let object = object::File::parse(&*mmap)?;
+        let ctx = addr2line::Context::new(&object)?;
+        let tree_root = Rc::new(RefCell::new(PProfRecordTreeNode::root()));
+        Ok(Self {
+            atsl_context: ctx,
+            tree_root: tree_root.clone(),
+            tree_node: tree_root.clone(),
+            tree_parent: vec![tree_root],
+        })
+    }
+}
+
+impl<'a, R: Register, M: Memory<R>, Inner: ckb_vm::machine::SupportMachine<REG = R, MEM = M>>
+    ckb_vm::machine::pprof::PProfLogger<ckb_vm::machine::DefaultMachine<'a, Inner>>
+    for PProfLogger
+{
+    fn on_step(&mut self, machine: &mut ckb_vm::machine::DefaultMachine<'a, Inner>) {
+        let pc = machine.pc().to_u64();
+        let decoder = ckb_vm::decoder::build_decoder::<R>(machine.isa(), machine.version());
+        let instruction = decoder.decode(machine.memory_mut(), pc).unwrap();
+        let cycles = machine
+            .instruction_cycle_func()
+            .as_ref()
+            .map(|f| f(instruction))
+            .unwrap_or(0);
+
+        let loc = self.atsl_context.find_location(pc).unwrap();
+        let loc_string = sprint_loc_file(&loc);
+        let frame_iter = self.atsl_context.find_frames(pc).unwrap();
+        let fun_string = sprint_fun(frame_iter);
+        let tag_string = format!("{}:{}", loc_string, fun_string);
+
+        self.tree_node = || -> Rc<RefCell<PProfRecordTreeNode>> {
+            if tag_string == self.tree_node.borrow().name {
+                return self.tree_node.clone();
+            }
+            if tag_string == String::from("??:??") {
+                self.tree_parent.clear();
+                return self.tree_root.clone();
+            }
+            if self.tree_parent.len() != 0 {
+                let p = self.tree_parent.last().unwrap().clone();
+                if tag_string == p.borrow().name {
+                    self.tree_parent.pop();
+                    return p;
+                }
+            }
+            let chd = Rc::new(RefCell::new(PProfRecordTreeNode {
+                name: tag_string,
+                childs: vec![],
+                cycles: 0,
+            }));
+            self.tree_node.borrow_mut().childs.push(chd.clone());
+            self.tree_parent.push(self.tree_node.clone());
+            return chd;
+        }();
+        self.tree_node.borrow_mut().cycles += cycles;
+    }
+
+    fn on_exit(&mut self, machine: &mut ckb_vm::machine::DefaultMachine<'a, Inner>) {
+        assert_eq!(machine.exit_code(), 0);
+        println!("{}", self.tree_root.borrow().display_flamegraph(""));
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let flag_parser = clap::App::new("ckb-vm-pprof")
+        .version("0.1")
+        .about("A pprof tool for CKB VM")
+        .arg(
+            clap::Arg::with_name("bin")
+                .long("bin")
+                .value_name("filename")
+                .help("Specify the name of the executable")
+                .required(true),
+        )
+        .get_matches();
+    let fl_bin = flag_parser.value_of("bin").unwrap();
+
+    let code_data = std::fs::read(fl_bin)?;
+    let code = bytes::Bytes::from(code_data);
+
+    let default_core_machine = ckb_vm::DefaultCoreMachine::<u64, ckb_vm::SparseMemory<u64>>::new(
+        ckb_vm::ISA_IMAC | ckb_vm::ISA_B,
+        ckb_vm::machine::VERSION1,
+        1 << 32,
+    );
+    let default_machine_builder = ckb_vm::DefaultMachineBuilder::new(default_core_machine)
+        .instruction_cycle_func(Box::new(cost_model::instruction_cycles));
+    let default_machine = default_machine_builder.build();
+    let pprof_func_provider = Box::new(PProfLogger::new(String::from(fl_bin))?);
+    let mut machine = ckb_vm::PProfMachine::new(default_machine, pprof_func_provider);
+    machine.load_program(&code, &vec!["main".into()]).unwrap();
+    machine.run()?;
+
+    Ok(())
+}
